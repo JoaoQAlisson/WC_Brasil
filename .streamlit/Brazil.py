525231bd67029c888e49912995063cd2146bcafd
João Alisson <joaoalisson.2908@gmail.com>
2026-06-12 12:46:25 -0300

    commit

--- .streamlit/Brazil.py
+++ .streamlit/Brazil.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import plotly_express as px
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import streamlit as st
 from datetime import date, timedelta
